84c34954c9784e26abaf522423adb2cf836bdfea
Alberto Rocha <betogrun@gmail.com>
2023-01-05 09:50:02 -0300

    Add the foundations for controller, serializer and test for user profile

---
+++ app/controllers/api/v1/controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module API
+  module V1
+    class Controller < ApplicationController
+      private
+
+      def render_json(result, serializer:, status: :ok)
+        render(json: serializer.new(result).as_json, status:)
+      end
+    end
+  end
+end
