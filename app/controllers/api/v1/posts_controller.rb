bc371b668dbea21b5c74136b4a2b203d1c0e92a8
Alberto Rocha <betogrun@gmail.com>
2023-01-08 09:47:30 -0300

    Refactor post params object to input object

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -4,9 +4,13 @@ module API
   module V1
     class PostsController < ::API::V1::Controller
       def index
-        ::Feed::RetrievePosts.call(params: ::Feed::Params.new(permitted_params))
+        ::Feed::RetrievePosts.call(params: ::Feed::Input.new(permitted_params))
           .on_success { |result| render_json(result[:posts], serializer: ::API::V1::PostsSerializer, status: :ok) }
           .on_failure(:invalid_params) { |result| render(json: result[:errors], status: :bad_request) }
+      end
+
+      def create
+        ::Feed::CreatePosts.call(params: ::Feed::CreatePosts::Params.new(permitted_params))
       end
 
       private
